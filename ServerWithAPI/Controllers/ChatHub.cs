3aeb9a87ef763a4d6118338322a9d2fcce0e744f
DRO3D <DRO3D@MSI>
2023-05-30 18:11:54 +0300

    Добавьте файлы проекта.

---
+++ ServerWithAPI/Controllers/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServerWithAPI.Controllers
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendUserMessage(string user, string message)
+        {
+            await Clients.User(user).SendAsync(message);
+        }
+
+        public async Task SendGroupMessage(string user, string message)
+        {
+
+        }
+    }
+
+}
